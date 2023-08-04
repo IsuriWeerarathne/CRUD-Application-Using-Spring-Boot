9fda45c78042155a852fa406481733e4ae381db9
IsuriWeerarathne <isuriweerarathna@gmail.com>
2023-08-04 10:25:31 +0530

    fields

--- MyWebApp/src/main/java/com/mycompany/MyWebApp/user/User.java
+++ MyWebApp/src/main/java/com/mycompany/MyWebApp/user/User.java
@@ -1,11 +1,22 @@
 package com.mycompany.MyWebApp.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 //jpa annotations
 @Entity
 @Table(name="users")
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //generate the id automatically in the database as an ID
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 50) // email should be not null and unique
+    private String email;
+
+    @Column(length = 15) // define the maximum length
+    private String password;
+
+    private String firstName;
+    private String lastName;
 }
