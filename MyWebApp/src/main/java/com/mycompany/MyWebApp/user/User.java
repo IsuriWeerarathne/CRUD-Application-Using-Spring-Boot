016d3a7ab6f2f48bf904db09391b4a222c60dd73
IsuriWeerarathne <isuriweerarathna@gmail.com>
2023-08-04 10:12:25 +0530

    users

---
+++ MyWebApp/src/main/java/com/mycompany/MyWebApp/user/User.java
@@ -0,0 +1,11 @@
+package com.mycompany.MyWebApp.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+//jpa annotations
+@Entity
+@Table(name="users")
+
+public class User {
+}
