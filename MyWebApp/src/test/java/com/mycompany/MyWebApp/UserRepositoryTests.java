5df14fdc1604dffa36d8b930b7c13d80be1a45cd
IsuriWeerarathne <isuriweerarathna@gmail.com>
2023-08-07 00:05:26 +0530

    annotations

--- MyWebApp/src/test/java/com/mycompany/MyWebApp/UserRepositoryTests.java
+++ MyWebApp/src/test/java/com/mycompany/MyWebApp/UserRepositoryTests.java
@@ -1,5 +1,8 @@
 package com.mycompany.MyWebApp;
 
+import com.mycompany.MyWebApp.user.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
@@ -8,5 +11,10 @@ import org.springframework.test.annotation.Rollback;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)//real db
 @Rollback(false)//committed to db
 public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
 
+    @Test
+    public void testAddNew(){
+
+    }
 }
