e771ca389a67a9e04c5a79d93dafd001a4292e6b
IsuriWeerarathne <isuriweerarathna@gmail.com>
2023-08-17 11:04:10 +0530

    add new user

--- MyWebApp/src/main/java/com/mycompany/MyWebApp/user/UserController.java
+++ MyWebApp/src/main/java/com/mycompany/MyWebApp/user/UserController.java
@@ -1,8 +1,8 @@
 package com.mycompany.MyWebApp.user;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; // Update the import statement
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -10,14 +10,14 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    @Autowired private UserService service;
+    @Autowired
+    private UserService service;
+
     @GetMapping("/users")
-    public String showList(Model model){
+    public String showList(Model model) {
         List<User> listUsers = service.listAll();
-        //model.addAttribute("listUsers",listUsers); addAttribute is showing as an error, why
-        
+        model.addAttribute("listUsers", listUsers);
+
         return "users";
     }
-
-
 }
